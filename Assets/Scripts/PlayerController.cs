5c2dbae49f44d87bced9e12de8a3e352aba22390
serhat-keskin <serhatkeskin710yedek@gmail.com>
2025-10-06 19:07:45 +0300

    Implemented horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 public class PlayerController : MonoBehaviour {
-    private void Start() { }
-    private void Update() { }
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private float moveX;
+    private void Start() { rb = GetComponent<Rigidbody2D>(); }
+    private void Update() { moveX = Input.GetAxis("Horizontal"); }
+    private void FixedUpdate() { rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y); }
 }
