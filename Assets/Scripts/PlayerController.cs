47f2fae14f41b94033a5949db92cca72c6701065
serhat-keskin <serhatkeskin710yedek@gmail.com>
2025-10-08 03:13:14 +0300

    Added jumping mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
     private Rigidbody2D rb;
     private float moveX;
     private void Start() { rb = GetComponent<Rigidbody2D>(); }
-    private void Update() { moveX = Input.GetAxis("Horizontal"); }
+    private void Update() { 
+        moveX = Input.GetAxis("Horizontal"); 
+        if (Input.GetButtonDown("Jump")) Jump();
+    }
     private void FixedUpdate() { rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y); }
+    private void Jump() { rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); }
 }
